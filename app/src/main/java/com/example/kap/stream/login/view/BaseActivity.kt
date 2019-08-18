3981b98399d7f6d08f58066d0b1e6cc3c6ceccba
Ara <ara.hakobyan@idt.am>
2019-08-18 18:24:11 +0400

    *

---
+++ app/src/main/java/com/example/kap/stream/login/view/BaseActivity.kt
@@ -0,0 +1,11 @@
+package com.example.kap.stream.login.view
+
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * Created by Ara Hakobyan on 8/18/19.
+ * company IDT
+ */
+abstract class BaseActivity : AppCompatActivity() {
+
+}
---
+++ app/src/main/java/com/example/kap/stream/login/view/LoginActivity.kt
@@ -0,0 +1,9 @@
+package com.example.kap.stream.login.view
+
+/**
+ * Created by Ara Hakobyan on 8/18/19.
+ * company IDT
+ */
+class LoginActivity : BaseActivity() {
+
+}
